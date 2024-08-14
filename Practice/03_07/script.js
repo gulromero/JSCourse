e11845a1221059540e8b7d520fc2085c57f25d4a
Gülsüm <gulsumerd38@gmail.com>
2024-08-14 21:18:40 +0200

    feat: exercise 1

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,15 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const book = {
+  name: "Head First JavaScript Programming",
+  year: 2014,
+  pages: 704
+};
+
+const cat = {
+  name: "Muezza",
+  age: 10,
+  breed: "Tortoise Shell"
+};
